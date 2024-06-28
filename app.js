aad273929a77594845fffb62a1304abcb115bc9f
kafymac <kafy5693@gmail.com>
2024-06-28 17:51:03 +0530

    build

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index'); // Correct path to index.js
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import cors
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DATABASE_URL)
+// Enable CORS for all routes
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
