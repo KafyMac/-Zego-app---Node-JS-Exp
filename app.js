6bfd1759e8e8cf6b36b53a1e1956473ce0b2c848
kafymac <kafy5693@gmail.com>
2024-07-09 15:29:26 +0530

    fix: notification added 4

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const http = require('http');
 const routes = require('./routes/index');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const { getFirestore } = require('firebase-admin/firestore');
 dotenv.config();
 
 const app = express();
@@ -12,6 +15,21 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
+
+
+
+//initialize firebase services
+const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS ? JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'base64').toString('utf8')) : null;
+
+if (serviceAccount && !admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        projectId: "zego-e33a6"
+    },
+        functions.config().firebase);
+
+    const firestore = getFirestore(admin.apps[0])
+}
 
 // Connect to MongoDB
 mongoose.connect(process.env.DATABASE_URL || "", {
